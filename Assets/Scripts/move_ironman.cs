12a693b82be79f733a3dc9cbe10e2079aabee470
Spalynx <Spalynx@gmail.com>
2017-09-24 02:04:51 -0500

    Added animations of ironman walking flying, etc to title screen. Also, added a background for title screen. And all that shit. gg no re.

---
+++ Assets/Scripts/move_ironman.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_ironman : MonoBehaviour {
+	public float speed = 1;
+	public GameObject iron;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speed++;	
+		 transform.Translate(Vector3.right * Time.deltaTime * speed/20);	
+	}
+}
