b8acb1b135ef21be0b140503da5b8a7d7a534f1c
Spalynx <Spalynx@gmail.com>
2017-10-13 02:09:00 -0500

    So, I think that I added bayesian logic to this, foll¯\_(ツ)_/¯ All I know is that it beats me pretty hard.

--- Assets/Scripts/RPS_game.cs
+++ Assets/Scripts/RPS_game.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using System;
 
 public class RPS_game : MonoBehaviour {
 	public Button ROCK, PAPER, SCISSORS;
-	public Text CPU_MOVE, P_WIN, C_WIN;
+	public Text CPU_MOVE, P_WIN, C_WIN, TURN;
 
 	private int p_win = 0, c_win = 0, cpu_move;
-	private int N = 0, i = -1, j;
-
+	private int i, j;
+	
 	// Use this for initialization
 	void Start () {
 		Button btn1 = ROCK.GetComponent<Button>();
@@ -23,79 +24,114 @@ public class RPS_game : MonoBehaviour {
 		btn3.onClick.AddListener(OnClick);
 	}
 	void Update(){
+		if(Input.GetKeyDown(KeyCode.Alpha1)){
+			player_move(0);			
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha2)){
+			player_move(1);			
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha3)){
+			player_move(2);			
+		}
 		P_WIN.text = p_win.ToString();
 		C_WIN.text = c_win.ToString();
+		TURN.text = "Turn: " + N;
 	}
 	
-	void player_move(int next_move){
-		N++;
-		j = next_move;
+	//0 = rock
+	//1 = paper 
+	//2 = scissor
 
-		if(i == -1){
-			CPU_MOVE.text = "ROCK";
-			cpu_move = 0;
+	int[] NAB = new int[9], AB = new int[2];
+	int[,] NCAB = new int[9,3];
+	double ProbRock = 0;
+	double ProbPaper = 0;
+	double ProbScissors = 0;
+	int Prediction = 0;
+	int RandomPrediction = 0;
+	int N = 0;
+	int NSuccess = 0;
+	
+	void player_move(int move){
+		int cpu_move = move_network(move);
+		
+		string move_text;
+		switch (cpu_move){
+			case 0:
+				move_text = "ROCK";
+				break;	
+			case 1:
+				move_text = "PAPER";
+				break;
+			case 2:
+				move_text = "SCISSOR";
+				break;
+			default:
+				move_text = "";	
+				break;
 		}
+		CPU_MOVE.text = move_text;  
 
-/*		
+		eval_move(move, cpu_move);			
+	}
+	int move_network(int move){
 		//HERE WE DEFINE THE BAYESIAN Network
 		//int i, j;
 		N++;
 		if(move == Prediction) NSuccess++;
-		if((AB[0] == Punch) && (AB[1] == Punch)) i = 0;
-		if((AB[0] == Punch) && (AB[1] == LowKick)) i = 1;
-		if((AB[0] == Punch) && (AB[1] == HighKick)) i = 2;
-		if((AB[0] == LowKick) && (AB[1] == Punch)) i = 3;
-		if((AB[0] == LowKick) && (AB[1] == LowKick)) i = 4;
-		if((AB[0] == LowKick) && (AB[1] == HighKick)) i = 5;
-		if((AB[0] == HighKick) && (AB[1] == Punch)) i = 6;
-		if((AB[0] == HighKick) && (AB[1] == LowKick)) i = 7;
-		http://ebooks.servegame.com/oreaiforgamdev475b/ch13_sect1_005.htm (3 of 6)7/24/05 1:26:16 AM
-		AI for Game Developers
-		if((AB[0] == HighKick) && (AB[1] == HighKick)) i = 8;
-		if(move == Punch) j = 0;
-		if(move == LowKick) j = 1;
-		if(move == HighKick) j = 2;
-		NAB[i]++;
-		NCAB[i][j]++;
-		AB[0] = AB[1];
-		AB[1] = move;
-		if((AB[0] == Punch) && (AB[1] == Punch)) i = 0;
-		if((AB[0] == Punch) && (AB[1] == LowKick)) i = 1;
-		if((AB[0] == Punch) && (AB[1] == HighKick)) i = 2;
-		if((AB[0] == LowKick) && (AB[1] == Punch)) i = 3;
-		if((AB[0] == LowKick) && (AB[1] == LowKick)) i = 4;
-		if((AB[0] == LowKick) && (AB[1] == HighKick)) i = 5;
-		if((AB[0] == HighKick) && (AB[1] == Punch)) i = 6;
-		if((AB[0] == HighKick) && (AB[1] == LowKick)) i = 7;
-		if((AB[0] == HighKick) && (AB[1] == HighKick)) i = 8;
-		ProbPunch = (double) NCAB[i][0] / (double) NAB[i];
-		ProbLowKick = (double) NCAB[i][1] / (double) NAB[i];
-		ProbHighKick = (double) NCAB[i][2] / (double) NAB[i];
-		if((ProbPunch > ProbLowKick) &&
-		(ProbPunch > ProbHighKick))
-		return Punch;
-		if((ProbLowKick > ProbPunch) &&
-		(ProbLowKick > ProbHighKick))
-		return LowKick;
-		if((ProbHighKick > ProbPunch) &&
-		(ProbHighKick > ProbLowKick))
-		return HighKick;
-		return (TStrikes) rand() % 3; // Last resort
-			
-		Example 13-3. Global variables
-		int NAB[9];
-		int NCAB[9][3];
-		TStrikes AB[2];
-		double ProbPunch;
-		double ProbLowKick;
-		double ProbHighKick;
-		TStrikes Prediction;
-		TStrikes RandomPrediction;
-		int N;
-		int NSuccess;
-*/	
-		eval_move(next_move,cpu_move);
 
+		if((AB[0] == 0) && (AB[1] == 0)) i = 0;
+		if((AB[0] == 0) && (AB[1] == 1)) i = 1;
+		if((AB[0] == 0) && (AB[1] == 2)) i = 2;
+		if((AB[0] == 1) && (AB[1] == 0)) i = 3;
+		if((AB[0] == 1) && (AB[1] == 1)) i = 4;
+		if((AB[0] == 1) && (AB[1] == 2)) i = 5;
+		if((AB[0] == 2) && (AB[1] == 0)) i = 6;
+		if((AB[0] == 2) && (AB[1] == 1)) i = 7;
+		if((AB[0] == 2) && (AB[1] == 2)) i = 8;
+
+		if(move == 0) j = 0;
+		if(move == 1) j = 1;
+		if(move == 2) j = 2;
+
+		//Iterate frequency of current move.
+		NAB[i]++;
+		NCAB[i,j]++;
+
+		//Flips current to previous move
+		AB[0] = AB[1];
+		
+		AB[1] = move;
+
+		//Sets i to the key of individual moves
+		if((AB[0] == 0) && (AB[1] == 0)) i = 0;
+		if((AB[0] == 0) && (AB[1] == 1)) i = 1;
+		if((AB[0] == 0) && (AB[1] == 2)) i = 2;
+		if((AB[0] == 1) && (AB[1] == 0)) i = 3;
+		if((AB[0] == 1) && (AB[1] == 1)) i = 4;
+		if((AB[0] == 1) && (AB[1] == 2)) i = 5;
+		if((AB[0] == 2) && (AB[1] == 0)) i = 6;
+		if((AB[0] == 2) && (AB[1] == 1)) i = 7;
+		if((AB[0] == 2) && (AB[1] == 2)) i = 8;
+
+		ProbRock = (double) NCAB[i,0] / (double) NAB[i];
+		ProbPaper = (double) NCAB[i,1] / (double) NAB[i];
+		ProbScissors = (double) NCAB[i,2] / (double) NAB[i];
+		
+		if((ProbRock > ProbPaper) &&
+				(ProbRock > ProbScissors))
+				return 0;
+		if((ProbPaper > ProbRock) &&
+				(ProbPaper > ProbScissors))
+				return 1;
+		if((ProbScissors > ProbRock) &&
+				(ProbScissors > ProbPaper))
+				return 2;
+
+		System.Random random = new System.Random();
+ 		return random.Next(0, 2);
+		// Last resort
+		
 	}
 	void eval_move(int player, int cpu){
 		if(player == cpu){
